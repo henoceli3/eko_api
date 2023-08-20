f5ae64d5f7766d2e9c3b894d49326586a9539d6e
KODJO TAMEGNON ELISEE HENOC <helitako16@gmail.com>
2023-08-20 22:00:35 +0000

    2

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 import express from "express";
 import createWallet from "./src/routes/v1/wallet/createWallet.js";
-import sendCryptoOnEthereum from "./src/routes/v1/Transation/Ethereum/Send.js";
+// import sendCryptoOnEthereum from "./src/routes/v1/Transation/Ethereum/Send.js";
 import estimateGasPrice from "./src/routes/v1/utils/getEThGasPrice.js";
 import sendCryptoOnSepolia from "./tests/testSepeolia.js";
 // import sendBitcoin from "./src/routes/v1/Transation/Bitcoin/send.js";
@@ -13,7 +13,7 @@ router.get("/", (req, res) => {
 
 router.get("/api/v1/createwallet", createWallet); // creation du wallet
 
-router.post("/api/v1/sendEthereum", sendCryptoOnEthereum); // transaction sur ethereum
+// router.post("/api/v1/sendEthereum", sendCryptoOnEthereum); // transaction sur ethereum
 router.get("/api/v1/getGasPrice", estimateGasPrice); // estimation du prix de transaction pour ethereum
 router.post("/api/v1/testSepolia", sendCryptoOnSepolia); // transaction sur sepolia  TODO : a supprimer
 
