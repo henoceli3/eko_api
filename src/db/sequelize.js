d8977ac28d47c8c2c81f340bc57ceb47cb5aa6aa
KODJO TAMEGNON ELISEE HENOC <helitako16@gmail.com>
2023-08-21 09:49:00 +0000

    6

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -6,8 +6,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Connexion à la base de données
-const sequelize = new Sequelize("eko", "root", "", {
-  host: "localhost",
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: "mysql",
   dialectModule: mysql2,
   dialectOptions: {
