d27534ab96a6ffc5b16fba6052790b6e030515b6
KODJO TAMEGNON ELISEE HENOC <helitako16@gmail.com>
2023-08-22 18:15:56 +0000

    17

--- src/routes/v1/wallet/getSecretPhrase.js
+++ src/routes/v1/wallet/getSecretPhrase.js
@@ -8,8 +8,8 @@ const getSecretPhrase = async (req, res) => {
         const walletGenerator = new WalletGenerator(
           process.env.ETHEREUM_PROVIDER_URL
         );
-        const secretPhrase = await walletGenerator.generateMnemonic(128);
-        res.status(200).json({ secretPhrase });
+        const mnemonic = await walletGenerator.generateMnemonic(128);
+        res.status(200).json({ mnemonic });
     } catch (error) {
         const message = `Une erreur est survenue : ${error}`;
         res.status(500).json({ message });
